886bf49bfd07e812a819baba6623d814b42cd195
TechMakerAI <154943877+techmakerai@users.noreply.github.com>
2024-01-12 16:00:17 -0500

    Add files via upload

---
+++ AI2Image2.py
@@ -0,0 +1,42 @@
+# Generate image from text by OpenAI's DALL-E Image API and then save it to a file
+# By TechMakerAI on YouTube 
+# 
+
+from PIL import Image
+import requests
+
+from openai import OpenAI
+client = OpenAI()
+
+# A text description of the desired image(s). 
+# The maximum length is 1000 characters for dall-e-2 
+# and 4000 characters for dall-e-3.
+text = "AI can generate an image from user input"
+  
+response = client.images.generate(
+  model = "dall-e-2",
+  prompt = text,
+  # Must be one of 256x256, 512x512, or 1024x1024 for dall-e-2. 
+  # Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models.
+  size = "1024x1024", 
+  quality = "standard", 
+  
+  # number of images. Must be between 1 and 10. 
+  # For dall-e-3, only n=1 is supported. 
+  n=1  
+)
+ 
+image_url = response.data[0].url
+
+im = Image.open(requests.get(image_url, stream=True).raw) 
+
+im.save("AIimage2.jpg","JPEG")
+
+# Display or save the generated image
+# print(f"Generated Image URL: {image_url}")
+
+
+ 
+
+
+ 
